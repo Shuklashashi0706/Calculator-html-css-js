6806db4b378ffa1851e0d0a22104f8baa8ed8128
Shashi-0706 <107978867+Shuklashashi0706@users.noreply.github.com>
2023-04-22 13:00:44 +0530

    commit on 22/04/23

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,7 @@
-"use script"
-let el= document.body.children;
-for(let key=0;key<el.length;key++){
-    console.log(key[i]);
+let doc=document.body.firstElementChild.firstElementChild.firstElementChild;
+f1=()=>{
+    doc.value=2;
 }
-document.getElementById("num-1").onclick()= ()=>{
-    let num=prompt("Enter number:");
-    document.getElementById("output").innerText=num;
+f=()=>{
+    doc.value=1;
 }
